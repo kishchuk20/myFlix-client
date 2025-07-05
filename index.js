02abb1a5e1a2437a769c1fe5aca7494d9d364a4c
kishchuk20 <issukk1237@gmail.com>
2025-07-04 22:04:35 -0400

    2.10

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 8080;
 
 const { check, validationResult } = require('express-validator');
 
+//mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
