816d8902c25642fe540c07f75597c435a97c7a39
kishchuk20 <issukk1237@gmail.com>
2025-07-14 23:13:55 -0400

    Update API URL and related components

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,10 +1,40 @@
-export const MovieView = ({ movie, onBackClick }) => (
-  <div>
-    <button onClick={onBackClick}>Back</button>
-    <h1>{movie.title}</h1>
-    <img src={movie.image} alt={movie.title} style={{ width: '200px' }} />
-    <p>{movie.description}</p>
-    <p><strong>Genre:</strong> {movie.genre}</p>
-    <p><strong>Director:</strong> {movie.director}</p>
-  </div>
-);
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const MovieView = ({ movie, onBackClick }) => {
+  const {
+    title = 'Movie Title Not Available',
+    description = 'Description Not Available',
+    image = 'https://via.placeholder.com/300x450?text=No+Image+Available',
+    genre = 'Genre Not Available',
+    director = 'Director Not Available'
+  } = movie || {};
+
+  console.log('Movie object:', movie);
+  console.log('Image URL:', image);
+
+  return (
+    <div className="movie-view">
+      <img src={image} alt={title} style={{ width: '300px' }} />
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p><strong>Genre:</strong> {genre}</p>
+      <p><strong>Director:</strong> {director}</p>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
+
+
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    imagePath: PropTypes.string,
+    genre: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
+
